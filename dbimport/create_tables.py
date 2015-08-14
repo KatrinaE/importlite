6619c1811a04888491528ae99a1b09d771e49f04
Katrina Ellison Geltman <katrina.m.ellison@gmail.com>
2015-08-14 15:52:15 -0400

    Add lookup_foreign_key()

--- dbimport/create_tables.py
+++ dbimport/create_tables.py
@@ -30,3 +30,19 @@ def import_all_rows(conn, tables, rows):
         conn.execute('ROLLBACK')
         raise
     conn.execute('COMMIT')
+
+
+def lookup_foreign_key(c, table, col_name, value):
+    foreign_table = foreign_key.table
+    foreign_col = table.foreign_keys[col_name]
+    sql = sqlgen.query_sql(foreign_table.name, foreign_col.name, value, 'id')
+    foreign_ids = c.execute(sql)
+
+    if len(foreign_ids) == 0:
+        msg = "{0} table has no entry '{1}' in the {2} column"
+        raise Exception(msg.format(foreign_table.name, row[csv_col], col_name))
+    elif len(foreign_ids) > 1:
+        msg = "{0} table has more than one row with {1} = {2}"
+        raise Exception(msg.format(foreign_table.name, col_name, row[csv_col]))
+    else:
+        return foreign_keys[0]
