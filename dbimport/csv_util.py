bdb42354cb0afc91ffec6ad12dcdbce2541293d5
Katrina Ellison Geltman <katrina.m.ellison@gmail.com>
2015-08-16 12:34:38 -0400

    Add get_destination_col() to csv_util

--- dbimport/csv_util.py
+++ dbimport/csv_util.py
@@ -19,3 +19,18 @@ def remove_commas_and_apostrophes(value):
 def scrub_row(row):
     return {csv_field: remove_commas_and_apostrophes(value)
             for csv_field, value in row.items()}
+
+
+def get_destination_col(table, csv_field):
+    destination_cols = [col for col in table.columns.values()
+                        if col.csv_name == csv_field]
+    if len(destination_cols) > 1:
+        raise Exception('Expected one column in {0} to import data from ' \
+                        'csv field {1}; more than one found.'
+                        .format(table.name, csv_field))
+    elif len(destination_cols) < 1:
+        raise Exception('Expected one column in {0} to import data from ' \
+                        'csv field {1}; none found.'
+                        .format(table.name, csv_field))
+    else:
+        return destination_cols[0]
